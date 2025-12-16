770abd54653f1877e376595069a847157c3b7da6
Warrior20025 <ahernandezguerrero582@gmail.com>
2025-12-16 12:42:56 +0100

    arreglo del sidebar en profile

--- FrontEnd/frontedReactNoteGym/src/routes/Profile.tsx
+++ FrontEnd/frontedReactNoteGym/src/routes/Profile.tsx
@@ -1,10 +1,16 @@
+import React, { useState, useEffect } from 'react';
 import HeaderGym from "../components/headerGym";
 import ProfileGym from "../components/ProfileGym";
+import Sidebar from "@/components/Sidebar";
+
+type UserRole = 'admin' | 'user' | 'trainer';
 
 export default function Profile() {
+  const [userRole, setUserRole] = useState<UserRole>('user'); 
   return (
     <div className="min-h-screen flex flex-col">
       <HeaderGym />
+      <Sidebar userRole={userRole} /> 
       <main className="flex-grow flex items-center justify-center p-4">
         <ProfileGym />
       </main>
