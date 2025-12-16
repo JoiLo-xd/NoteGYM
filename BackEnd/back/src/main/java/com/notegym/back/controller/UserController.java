1a30368d86e2ce8f440fb0ce23670022b1dfa997
joel lopez <joel.lopez.encinas@gmail.com>
2025-12-16 14:28:36 +0100

    añadiendo sistema precoz de logs

--- BackEnd/back/src/main/java/com/notegym/back/controller/UserController.java
+++ BackEnd/back/src/main/java/com/notegym/back/controller/UserController.java
@@ -3,6 +3,8 @@ package com.notegym.back.controller;
 import java.security.Timestamp;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -34,6 +36,8 @@ public class UserController {
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
 
+    private static final Logger AUDIT_LOGGER = LoggerFactory.getLogger("com.notegym.back.auditor");
+
 
     public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder){
         this.userRepository = userRepository;
@@ -53,9 +57,11 @@ public class UserController {
         newUser.setUsername(newUser.getUsername().toLowerCase());
         String username = newUser.getUsername();
         if (userRepository.existsById(username)){
+            AUDIT_LOGGER.warn("REGISTER_FAILED: Aquest usuari ja esta registrat: {}", newUser.getUsername());
             throw new ResponseStatusException(HttpStatus.CONFLICT, "Este usuario ya esta registrado, no puedes añadirlo");
         }
         else if (userRepository.existsByMail(newUser.getMail())){
+            AUDIT_LOGGER.warn("LOGIN_FAILED: Aquest email ja esta actualitzat: {}", newUser.getUsername());
             throw new ResponseStatusException(HttpStatus.CONFLICT, "Este email ya esta siendo utilizado");
 
         }
@@ -77,12 +83,14 @@ public class UserController {
         //El frontEnd lo deberia pasar en lowercase...
         login.setUsername(login.getUsername().toLowerCase());
         if (!userRepository.findByUsername(login.getUsername()).isPresent()){
+            AUDIT_LOGGER.warn("LOGIN_FAILED: Usuario no encontrado: {}", login.getUsername());
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No se ha encontrado nigun usuario con ese nombre");
         }   
 
         User usuario = userRepository.findByUsername(login.getUsername()).get();
         User userWithoutEncript = usuario;
         if (usuario.isBlocked()){
+            AUDIT_LOGGER.warn("LOGIN_FAILED: Este usuari esta bloquejat: {}", login.getUsername());
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Aquest usuari esta bloquejat"); 
 
         }
@@ -92,10 +100,14 @@ public class UserController {
             if (usuario.getTriesLogIn() == 3){
                 usuario.setBlocked( true);
                 userRepository.save(usuario);
+                AUDIT_LOGGER.warn("LOGIN_FAILED: Contraseya incorrecta, compte bloquejat: {}", login.getUsername());
                 throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "La contrasenya no es adecuada, ja has intentat els 3 intents, el compte " + usuario.getUsername() + " ha quedat bloqueixat"); 
             }
             userRepository.save(usuario);
+            AUDIT_LOGGER.warn("LOGIN_FAILED: Contraseya incorrecta: {}", login.getUsername());
+
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "La contrasenya no es adecuada"); 
+            
         }
         usuario.setTriesLogIn(0);
         userRepository.save(usuario);
@@ -110,18 +122,22 @@ public class UserController {
         
         login.setUsername(login.getUsername().toLowerCase());
         if (!userRepository.findByUsername(login.getUsername()).isPresent()){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No se ha encontrado nigun usuario con ese nombre");
+            AUDIT_LOGGER.warn("DESBLOCK_FAILED: Usuari Admin no trobat: {}", login.getUsername());
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No se ha encontrado nigun usuario admin con ese nombre");
         }
         User usuario = userRepository.findByUsername(login.getUsername()).get();
         if (!passwordEncoder.matches(login.getPassword(), usuario.getPassword())){
+            AUDIT_LOGGER.warn("DESBLOCK_FAILED: Contraseya admin incorrecta: {}", login.getUsername());
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "La contrasenya no es adecuada"); 
         }
 
         if (!usuario.getRole().equals("admin")){
+            AUDIT_LOGGER.warn("DESBLOCK_FAILED: El usuari no es admin: {}", login.getUsername());
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Este usuario no es administrador"); 
         }
 
         if (!userRepository.findByUsername(username).isPresent()){
+            AUDIT_LOGGER.warn("DESBLOCK_FAILED: No es troba ningun usuari amb aquest nom per cambiar l'estat: {}", login.getUsername());
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No se ha encontrado nigun usuario con ese nombre al que intentas cambiar el estado");
         }
     
